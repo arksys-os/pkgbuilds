cdeaed297dbe16582740221a48aeb976883cca25
David7ce <david7ce@outlook.com>
2023-08-26 18:07:10 +0100

    update

--- build-dir.sh
+++ build-dir.sh
@@ -6,20 +6,20 @@ folders=("arcolinux-calamares-3.3.0-alpha6-01" "arksys-calamares-config" "arksys
 # Display the available folders for selection
 echo "Select a folder:"
 for ((i=0; i<${#folders[@]}; i++)); do
-    echo "$i. ${folders[i]}"
+    echo "$((i+1)). ${folders[i]}"
 done
 
 # Read user's choice
 read -p "Enter the number corresponding to the folder: " choice
 
 # Validate user's input
-if [[ ! "$choice" =~ ^[0-9]+$ ]] || ((choice < 0)) || ((choice >= ${#folders[@]})); then
+if [[ ! "$choice" =~ ^[0-9]+$ ]] || ((choice < 0)) || ((choice >= ${#folders[@]}+1)); then
     echo "Invalid choice. Exiting."
     exit 1
 fi
 
-# Get the selected folder
-selected_folder="${folders[choice]}"
+# Adjust the choice to array index (subtracting 1)
+selected_folder="${folders[choice - 1]}"
 
 # Navigate to the selected folder
 cd $selected_folder || exit 1
@@ -32,7 +32,7 @@ read -p "Do you want to remove files generated in makepkg? [Y/n] " answer
 
 if [[ ${answer,,} =~ ^(yes|y)$ ]]; then
     echo "Executing the command..."
-    rm -rf ./pkg/ ./src/
+    rm -rf $selected_folder ./pkg/ ./src/
 else
     echo "Command not executed."
 fi
